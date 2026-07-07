a8821b512b551fe48630b6eeadaa60e4861285f0
pourya <pouryathesaymon@gmail.com>
2026-07-07 17:56:22 +0330

    notif service requirement added to config

--- app/config/load.py
+++ app/config/load.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    TELEGRAM_BOT_TOKEN: str
+    TELEGRAM_CHAT_ID: str
 
     class Config:
         env_file = ".env"
