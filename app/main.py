beeaedc3b716ce40359578e79de2d402f4e7e266
pourya <pouryathesaymon@gmail.com>
2026-07-08 03:32:15 +0330

    connecting to back started, .env.example added

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import Base
 from app.database import engine
@@ -7,5 +8,13 @@ from app.routes import router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router, prefix="/api")
